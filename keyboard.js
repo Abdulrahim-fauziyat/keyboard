054d4da997ac085ad86ab51ff1bc66b76218eccb
Abdulrahim-fauziyat <omoteabdulrahimfauziyat@gmail.com>
2024-02-26 14:49:24 +0100

    first commit

---
+++ keyboard.js
@@ -0,0 +1,40 @@
+
+
+
+
+   let num=button;
+    let input=document.getElementById (inputBox);
+    let button= document.querySelectorAll(button);
+
+    function myfunction{
+        document.addEventListener(click)
+         
+  
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
